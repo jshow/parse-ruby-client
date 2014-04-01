e111ee944719eb2092328260404f1e8aa98a07a1
Milton Andrade <miligraf@gmail.com>
2014-04-01 14:28:48 -0400

    add Parse::Installation.delete; fix installation object used for testing

--- test/test_installation.rb
+++ test/test_installation.rb
@@ -1,13 +1,13 @@
 require 'helper'
 
-class TestModelObject < Parse::Installation
+class TestInstallationObject < Parse::Installation
 end
 
 class TestInstallation < ParseTestCase
 
   def test_new
     VCR.use_cassette('test_new_installation', :record => :new_episodes) do
-      tmo = TestModelObject.new
+      tmo = TestInstallationObject.new
       assert_equal tmo.new?, true
       tmo.save
       assert_equal tmo.new?, false
@@ -16,8 +16,8 @@ class TestInstallation < ParseTestCase
 
   def test_save
     VCR.use_cassette('test_new_installation', :record => :new_episodes) do
-      tmo = TestModelObject.new
-      installation = TestModelObject.new
+      tmo = TestInstallationObject.new
+      installation = TestInstallationObject.new
       installation.device_type = 'ios'
       installation.device_token = 'yaba_daba_do'
       installation.channels = ['bambam', 'joe rockhead']
@@ -29,18 +29,44 @@ class TestInstallation < ParseTestCase
 
   def test_get
     VCR.use_cassette('test_get_installation', :record => :new_episodes) do
-      installation = TestModelObject.new
+      installation = TestInstallationObject.new
       installation.device_type = 'ios'
       installation.device_token = 'yaba_daba_do'
       installation.channels = ['bambam', 'joe rockhead']
       installation.save
 
-      i = TestModelObject.get(installation.id)
+      i = TestInstallationObject.get(installation.id)
 
       assert_equal i.id, installation.id
       assert_equal i["device_type"], installation["device_type"]
       assert_equal i["device_token"], installation["device_token"]
       assert_equal i["channels"], installation["channels"]            
+    end
+  end
+
+  def test_delete
+    VCR.use_cassette('test_get_installation', :record => :new_episodes) do
+      installation = TestInstallationObject.new
+      installation.device_type = 'ios'
+      installation.device_token = 'yaba_daba_do'
+      installation.channels = ['bambam', 'joe rockhead']
+      installation.save
+
+      i = TestInstallationObject.get(installation.id)
+
+      assert_equal i.id, installation.id
+      assert_equal i["device_type"], installation["device_type"]
+      assert_equal i["device_token"], installation["device_token"]
+      assert_equal i["channels"], installation["channels"]
+
+      TestInstallationObject.delete(installation.id)
+
+      i = TestInstallationObject.get(installation.id)
+
+      assert_equal i.id, nil
+      assert_equal i["device_type"], nil
+      assert_equal i["device_token"], nil
+      assert_equal i["channels"], nil
     end
   end
 end
